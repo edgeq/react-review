68958f9796484d8b449f38d8e729f2cb0d1fc6a2
edgeq <edgarq86@gmail.com>
2019-06-05 16:46:29 -0500

    set up logic for game and conditional renders based on win state

--- 05-pokedex/src/Pokecard.js
+++ 05-pokedex/src/Pokecard.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Pokecard.css'
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API = ' https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+//take a number less than 100 and add two 0s in front of it, then slice the number from the last 3 digits and return that new number.
+let padToThree = (number) => (number <= 99 ? `00${number}`.slice(-3): number)
 
 class Pokecard extends Component {
   render() {
-  console.log(this.props)
-  let imgSrc = `${POKE_API}${this.props.id}.png`
+  // console.log(this.props)
+  let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
    return (
     <div className="Pokecard">
       <h1 className="Pokecard-title">{this.props.name}</h1>
