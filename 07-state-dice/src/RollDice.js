cc4775098f7548287fe87fa3f5f31add470dbf71
edgeq <edgarq86@gmail.com>
2019-06-06 17:19:39 -0500

    roll of the dice!

---
+++ 07-state-dice/src/RollDice.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Dice from './Dice'
+
+class RollDice extends Component {
+  
+  render(){
+    return (
+      <div className="RollDice">
+
+        <Dice />
+      </div>
+    )
+  }
+}
+
+
+export default RollDice;
