79aa085cdda846f4869c1967cca6fa967c553c9d
edgeq <edgarq86@gmail.com>
2019-06-06 17:31:13 -0500

    refactored for modularity and encapsulation

--- 07-state-dice/src/Dice.js
+++ 07-state-dice/src/Dice.js
@@ -1,37 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Dice extends Component {
-  static defaultProps = {
-    dieFace : ['one', 'two', 'three', 'four', 'five', 'six']
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      diceFace01 : 'fas fa-dice-one',
-      diceFace02 : 'fas fa-dice-six',
-    }
-  }
-
-  roll = (e) => {
-    let faClass = 'fas fa-dice-';
-    let dieVal01 = Math.floor(Math.random() * this.props.dieFace.length);
-    let dieVal02 = Math.floor(Math.random() * this.props.dieFace.length);
-    let diceVal01 = faClass + this.props.dieFace[dieVal01];
-    let diceVal02 = faClass + this.props.dieFace[dieVal02];
-    this.setState({diceFace01 : diceVal01, diceFace02 : diceVal02});
-
-  }
 
   render() {
     return (
       <div className="Dice">
-
-        <h1><i className={this.state.diceFace01}></i><i className={this.state.diceFace02}></i></h1>
-
-        <button className="btn" onClick={this.roll}>Roll Dice</button>
+        <h1>
+          <i className={this.props.die01} />
+          <i className={this.props.die02} />
+        </h1>
       </div>
-    )
+    );
   }
 }
 
--- 07-state-dice/src/RollDice.js
+++ 07-state-dice/src/RollDice.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
-import Dice from './Dice'
+import React, { Component } from "react";
+import Dice from "./Dice";
 
 class RollDice extends Component {
-  
-  render(){
+  static defaultProps = {
+    dieFace: ["one", "two", "three", "four", "five", "six"]
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      diceFace01: "fas fa-dice-one",
+      diceFace02: "fas fa-dice-six"
+    };
+  }
+
+  roll = e => {
+    let faClass = "fas fa-dice-";
+    let dieVal01 = Math.floor(Math.random() * this.props.dieFace.length);
+    let dieVal02 = Math.floor(Math.random() * this.props.dieFace.length);
+    let diceVal01 = faClass + this.props.dieFace[dieVal01];
+    let diceVal02 = faClass + this.props.dieFace[dieVal02];
+    this.setState({ diceFace01: diceVal01, diceFace02: diceVal02 });
+  };
+  render() {
     return (
       <div className="RollDice">
+        <Dice die01={this.state.diceFace01} die02={this.state.diceFace02} />
 
-        <Dice />
+        <button className="btn" onClick={this.roll}>
+          Roll Dice
+        </button>
       </div>
-    )
+    );
   }
 }
-
 
 export default RollDice;
