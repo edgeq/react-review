d0ee85fa872305eb937a97497ea2d1c917a6dfcf
edgeq <edgarq86@gmail.com>
2019-07-02 15:30:21 -0500

    first foray into using chroma js

--- 18-material-palettes/src/App.js
+++ 18-material-palettes/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import  Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+    return (
     <div className="App">
       {/* spread a palette to unpack it's contents as props */}
       <Palette {...seedColors[6]}/>
     </div>
   );
 }
+}
+
 
 export default App;
