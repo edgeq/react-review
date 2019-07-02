22f13e788205311dd3e6964afc1733c630317d1c
edgeq <edgarq86@gmail.com>
2019-07-02 13:46:12 -0500

    added the CopyToClipboard library

--- 18-material-palettes/src/ColorBox.js
+++ 18-material-palettes/src/ColorBox.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends Component {
   render() {
     const { name, background } = this.props;
     return (
-      // this.props comes from Palette
+      <CopyToClipboard text={background}>
+
+      {/* this.props comes from Palette */}
       <div className='ColorBox' style={{ background }}>
         <div className="copy-container">
           <div className="box-content">
@@ -15,6 +18,7 @@ class ColorBox extends Component {
         </div>
         <span className="see-more">More</span>
       </div>
+      </CopyToClipboard>
     );
   }
 }
