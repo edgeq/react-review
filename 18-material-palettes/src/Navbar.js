25d3678266ccc6e1f3da993a5d02b8a1f476f4b2
edgeq <edgarq86@gmail.com>
2019-07-03 12:36:01 -0500

    added material UI and got hex select working

--- 18-material-palettes/src/Navbar.js
+++ 18-material-palettes/src/Navbar.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import {Select, MenuItem} from '@material-ui/core';
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import './Navbar.css';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {format: "hex"};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({format: e.target.value});
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const {format} = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -22,6 +34,13 @@ class Navbar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX VALUE GOES HERE</MenuItem>
+            <MenuItem value="rgb">RGB VALUE GOES HERE</MenuItem>
+            <MenuItem value="rgba">RGBA VALUE GOES HERE</MenuItem>
+          </Select>
         </div>
       </header>
     );
--- 18-material-palettes/src/Palette.js
+++ 18-material-palettes/src/Palette.js
@@ -6,26 +6,31 @@ import "./Palette.css";
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
     console.log(level);
   }
+
+  changeFormat(val){
+    this.setState({format: val});
+  }
   render() {
     // this.props is coming from App.js import of seedColors.js
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
 
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} key={color.name}/>
+      <ColorBox background={color[format]} name={color.name} key={color.name}/>
     ));
     return (
       <div className="Palette">
         {/* Navbar goes here */}
-        <Navbar level={level} changeLevel={this.changeLevel}/>
+        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
         <div className="Palette-colors">
           {/* bunch of color boxes */}
           {colorBoxes}
