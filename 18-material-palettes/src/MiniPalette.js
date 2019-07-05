9041d9954ab547036be249d0e72640d3bb8f0ac4
Edge Quintanilla <edgarq86@gmail.com>
2019-07-05 13:08:19 -0500

    more JSS styling

--- 18-material-palettes/src/MiniPalette.js
+++ 18-material-palettes/src/MiniPalette.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import { withStyles } from '@material-ui/styles';
+import React from "react";
+import { withStyles } from "@material-ui/styles";
 
 // styles is an object comprised of classNames
 const styles = {
   // each className is an onject with properties
-  main: {
-    backgroundColor: "purple",
-    border: "3px solid teal"
+  root: {
+    backgroundColor: "white",
+    border: "1px solid black",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    "&:hover" : {
+      cursor: "pointer"
+    }
   },
-  secondary: {
-    backgroundColor: "pink"
+  colors: {
+    backgroundColor: "grey"
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative"
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem"
   }
 };
 
@@ -17,15 +38,17 @@ function MiniPalette(props) {
   // we use props because we're in a functional component and NOT a class.
   // ...styles will be packaged inside of { classes } on export
   // { classes } is extracted from withStyles.
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
   console.log(classes);
 
   return (
-    <div className={classes.main}>
-      <h3>Mini Palette</h3>
-      <section className={classes.secondary}>lajnsd ;alsdn l;kmn </section>
+    <div className={classes.root}>
+      <div classname={classes.colors} />
+      <h5 className={classes.title} aria-label={paletteName}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
-  )
+  );
 }
 
 // Component Inception:
