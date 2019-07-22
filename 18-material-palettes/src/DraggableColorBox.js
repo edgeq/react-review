10678f851930fdebe2201c27a57d922085487a24
edgeq <edgarq86@gmail.com>
2019-07-22 17:02:33 -0500

    moved inline JSS to their own files. added constants file

--- 18-material-palettes/src/DraggableColorBox.js
+++ 18-material-palettes/src/DraggableColorBox.js
@@ -2,38 +2,7 @@ import React from "react";
 import { SortableElement } from "react-sortable-hoc";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { withStyles } from "@material-ui/styles";
-
-const styles = {
-  root: {
-    width: "20%",
-    height: "25%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-3.5px",
-    "&:hover svg": {
-      color: "white",
-      transform: "scale(1.25)"
-    }
-  },
-  boxContent: {
-    display: "flex",
-    justifyContent: "space-between",
-    position: "absolute",
-    width: "100%",
-    left: "0px",
-    bottom: "0px",
-    padding: "10px",
-    color: "rgba(0, 0, 0, 0.5)",
-    letterSpacing: "1px",
-    textTransform: "uppercase",
-    fontSize: "12px"
-  },
-  deleteIcon: {
-    transition: "all 0.3s ease-in-out"
-  }
-};
+import styles from "./styles/DraggableColorBox";
 
 const DraggableColorBox = SortableElement(props => {
   const { classes, color, name, handleRemove } = props;
