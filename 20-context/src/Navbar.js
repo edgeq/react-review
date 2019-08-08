ed9706cebb8010a08f662d492b572059327879e3
edgeq <edgarq86@gmail.com>
2019-08-08 13:40:23 -0500

    context in class based components complete

--- 20-context/src/Navbar.js
+++ 20-context/src/Navbar.js
@@ -10,8 +10,24 @@ import {
 } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import { ThemeContext } from "./contexts/ThemeContext";
+import { withLanguageContext } from "./contexts/LanguageContext";
 
 import styles from "./styles/NavbarStyles";
+
+const content = {
+  english: {
+    search: "Search",
+    flag: "🇬🇧"
+  },
+  french: {
+    search: "Chercher",
+    flag: "🇫🇷"
+  },
+  spanish: {
+    search: "Buscar",
+    flag: "🇪🇸"
+  }
+};
 
 class Navbar extends Component {
   //tells component to look up the component tree for a context
@@ -19,17 +35,19 @@ class Navbar extends Component {
   render() {
     const { isDarkMode, toggleTheme } = this.context;
     const { classes } = this.props;
+    const { language } = this.props.languageContext;
+    const { search, flag } = content[language];
     return (
       <div className={classes.root}>
         <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
           <Toolbar>
             <IconButton className={classes.menuButton} color="inherit">
               <span role="img" aria-label="country-flag">
-                🇫🇷
+                {flag}
               </span>
             </IconButton>
             <Typography className={classes.title} variant="h6" color="inherit">
-              App Title
+              App Title - {language}
             </Typography>
             <Switch onChange={toggleTheme} />
             <div className={classes.grow} />
@@ -38,7 +56,7 @@ class Navbar extends Component {
                 <SearchIcon />
               </div>
               <InputBase
-                placeholder="Search..."
+                placeholder={`${search}...`}
                 classes={{
                   root: classes.inputRoot,
                   input: classes.inputInput
@@ -52,4 +70,4 @@ class Navbar extends Component {
   }
 }
 
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
